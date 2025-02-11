47a3233a5aef9c5352de82855a49364f852a8521
lichanda <choeunml@gmatc.matc.edu>
2025-02-11 08:54:48 -0600

    Saving changes

---
+++ blog/.cache/sync-requires.js
@@ -0,0 +1,17 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\code\\matc\\itdev164\\lichanda-choeunmunesy\\blog\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\code\\matc\\itdev164\\lichanda-choeunmunesy\\blog\\src\\pages\\404.js")),
+  "component---src-pages-about-js": preferDefault(require("C:\\code\\matc\\itdev164\\lichanda-choeunmunesy\\blog\\src\\pages\\about.js")),
+  "component---src-pages-contact-js": preferDefault(require("C:\\code\\matc\\itdev164\\lichanda-choeunmunesy\\blog\\src\\pages\\contact.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\code\\matc\\itdev164\\lichanda-choeunmunesy\\blog\\src\\pages\\index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("C:\\code\\matc\\itdev164\\lichanda-choeunmunesy\\blog\\src\\pages\\page-2.js")),
+  "component---src-pages-using-ssr-js": preferDefault(require("C:\\code\\matc\\itdev164\\lichanda-choeunmunesy\\blog\\src\\pages\\using-ssr.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("C:\\code\\matc\\itdev164\\lichanda-choeunmunesy\\blog\\src\\pages\\using-typescript.tsx")),
+  "component---src-templates-blog-post-js": preferDefault(require("C:\\code\\matc\\itdev164\\lichanda-choeunmunesy\\blog\\src\\templates\\blog-post.js"))
+}
+
