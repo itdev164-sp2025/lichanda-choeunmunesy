73aaf52805f2514b9339785eef89cd685f22390e
lichanda <choeunml@gmatc.matc.edu>
2025-03-27 09:31:55 -0500

    Saving Changes

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -7,8 +7,8 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import { Gray } from './themes/Gray'
+import Header from "./Header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
