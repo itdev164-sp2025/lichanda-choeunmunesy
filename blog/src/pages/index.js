2d0bbc73a4535d943a4dc6f4ab9b4b82e8d6a03a
lichanda <choeunml@gmatc.matc.edu>
2025-03-27 09:54:44 -0500

    Saving Changes

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -5,15 +5,16 @@ import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { List, ListItem } from '../components/List'
 import * as styles from "../components/index.module.css"
 
 const IndexPage = ({data}) => (
   <Layout>
     <Seo title="Home"/>
-   <ul className={styles.list}>
+   <List width={[1, 2/3, 7/8]} p={2}>
   {
     data.allContentfulBlogPost.edges.map(edge => (
-      <li key={edge.node.id}>
+      <ListItem p={3} key={edge.node.id}>
         <Link to={edge.node.slug}>{edge.node.title}</Link>
         <div>
           <GatsbyImage
@@ -23,10 +24,10 @@ const IndexPage = ({data}) => (
         <div>
           {edge.node.body.childMarkdownRemark.excerpt}
         </div>
-      </li>
+        </ListItem>
     ))
   }
-</ul>
+</List>
   </Layout>
 )
 
@@ -35,7 +36,7 @@ const IndexPage = ({data}) => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-
+export const head = () => <Seo title="Home" />
 
 export default IndexPage
 
@@ -56,7 +57,7 @@ export const query = graphql `
           gatsbyImageData(
             layout: CONSTRAINED
             placeholder: BLURRED
-            width: 300
+            width: 600
           )
         }
       }
