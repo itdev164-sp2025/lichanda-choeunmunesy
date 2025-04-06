cd110e9cc892d05ebb73d3697e9c896272966aad
lichanda <choeunml@gmatc.matc.edu>
2025-04-06 11:17:42 -0500

    saving changes

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from 'gatsby';  
+import { H1 } from "../Heading"
 
 const StyledHeader = styled.header`
   background: #f8f8f8;
@@ -16,7 +17,11 @@ const StyledLink = styled(Link)`
 
 const Header = ({ siteTitle }) => (
   <StyledHeader>
-    <StyledLink to="/">{siteTitle}</StyledLink>
+    <StyledLink to="/">
+    <H1>
+    {siteTitle}
+    </H1>
+    </StyledLink>
   </StyledHeader>
 );
 
---
+++ blog/src/components/Heading/Heading1.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { BaseHeading } from './BaseHeading'
+
+const Heading1 = props => 
+    <BaseHeading as='h1' font-size={[3, 4, 5]} {...props}/>
+
