9680693ec03b917e216ce12c042377ed5f800e61
KOJO0216 <choeunlanna@gmail.com>
2025-04-08 19:20:32 -0500

    Completed Activity 8

---
+++ blog/src/components/themes/Gray/Gray.js
@@ -0,0 +1,41 @@
+const theme = {
+    space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+    colors: {
+      grays: [ 
+        'hsl(208, 13%, 100%)',
+        'hsl(208, 13%, 90%)',
+        'hsl(208, 13%, 80%)',
+        'hsl(208, 13%, 75%)',
+        'hsl(208, 13%, 65%)',
+        'hsl(208, 13%, 55%)',
+        'hsl(208, 13%, 45%)',
+        'hsl(208, 13%, 35%)',
+        'hsl(208, 13%, 25%)',
+        'hsl(208, 13%, 15%)',
+        'hsl(208, 13%, 5%)',
+      ]
+    }
+  };
+  
+  const variants = {
+    iconButton: {
+      primary: {
+        color: theme.colors.grays[8]
+      },
+      contrast: {
+        color: theme.colors.grays[0]
+      }
+    },
+    header: {
+      primary: {
+       
+        backgroundColor: theme.colors.grays[8],
+        color: theme.colors.grays[0]
+      }
+    }
+  };
+  
+  export const Gray = {
+    ...theme,
+    variants
+  };
